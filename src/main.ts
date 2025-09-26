3f0fc8f0897b849b07538345f747854687d92475
TieR89 <tier8@yandex.ru>
2025-09-26 04:24:26 +0300

    feat(deploy): add vercel.json and update CORS for production

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,12 @@ async function bootstrap() {
 
   // Включаем CORS с конкретным origin (для безопасности; замените на '*' для всех, но не в prod)
   app.enableCors({
-    origin: 'http://localhost:5173', // Origin вашего frontend (Vite dev server)
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Разрешенные методы
-    credentials: true, // Если нужны cookies/auth
+    origin: [
+      'http://localhost:5173',
+      'https://TieR89.github.io/vue-forms-app/',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
   });
 
   await app.listen(3000);
